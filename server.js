b183c855fcb4a95c9933217386f021c80e482bb4
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-08-03 12:01:48 -0700

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrackerdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrackerdb", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
+});
 
 // routes
 app.use(require("./routes/api.js"));
